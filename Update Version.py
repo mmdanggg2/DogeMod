d87d736533ffc66ddc3c1d804ce3c56eda596fe6
James Horsley <jbhorsley@gmail.com>
2014-02-23 17:01:16 +0000

    Added a version number updater script

---
+++ Update Version.py
@@ -0,0 +1,22 @@
+import json
+import re
+
+def updatefile(filename, ver):
+	lines = open(filename).readlines()
+
+	for i, line in enumerate(lines):
+		if 'public static final String VER = "' in line:
+			lines[i] = '	public static final String VER = "%s";\n' % newVer
+	
+	open(filename, 'w').write(''.join(lines))
+
+
+newVer = raw_input('Version number: ')
+
+modinfo = json.load(open('mcmod.info'))
+
+modinfo['version'] = newVer
+
+json.dump(modinfo, open('mcmod.info', 'w'), indent = 4)
+
+updatefile('mmdanggg2\\doge\\BasicInfo.java', newVer)
