1e0f1ff15e49490ce5d16d7b90198c582e3bad45
James Horsley <jbhorsley@gmail.com>
2014-07-22 19:32:50 +0100

    Version 0.5.0

--- mmdanggg2/doge/BasicInfo.java
+++ mmdanggg2/doge/BasicInfo.java
@@ -4,7 +4,7 @@ public class BasicInfo {
 	
 	public static final String ID = "Doge";
 	public static final String NAME = "Doge";
-	public static final String VER = "0.4.3";
+	public static final String VER = "0.5.0";
 	public static final String CLIENTPROXY = "mmdanggg2.doge.client.";
 	public static final String COMMONPROXY = "mmdanggg2.doge.";
 	
