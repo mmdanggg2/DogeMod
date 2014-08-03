f4e52ab52d0d40109aa674a91ec1f2a3321f1936
James Horsley <jbhorsley@gmail.com>
2014-08-03 03:41:49 +0100

    Added proper texture to MiningRig Made the MiningRig 'rotate' its texture

--- mmdanggg2/doge/blocks/MiningRig.java
+++ mmdanggg2/doge/blocks/MiningRig.java
@@ -1,16 +1,21 @@
 package mmdanggg2.doge.blocks;
 
-import mmdanggg2.doge.DogeInfo;
 import mmdanggg2.doge.Doge;
+import mmdanggg2.doge.DogeInfo;
 import mmdanggg2.doge.blocks.tileentities.MiningRigTileEntity;
+import mmdanggg2.doge.util.DogeLogger;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import cpw.mods.fml.common.network.internal.FMLNetworkHandler;
 
@@ -19,11 +24,10 @@ public class MiningRig extends BlockContainer {
 	public MiningRig(Material material) {
 		super(material);
 		
-		setHardness(1.0f);
+		setHardness(2.0f);
 		setStepSound(Block.soundTypeMetal);
 		setBlockName("miningRig");
 		setCreativeTab(Doge.dogeTab);
-		setBlockTextureName(DogeInfo.NAME.toLowerCase() + ":miningRig");
 	}
 
 	@Override
@@ -35,6 +39,30 @@ public class MiningRig extends BlockContainer {
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
 		FMLNetworkHandler.openGui(player, Doge.instance, 0, world, x, y, z);
 		return true;
+	}
+	
+	@Override
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack stack) {
+		int rotation = MathHelper.floor_double(entity.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
+		if (!world.isRemote) {
+			DogeLogger.logDebug("MiningRig Placed, meta = " + rotation);
+		}
+		
+		if (rotation == 0) {
+			world.setBlockMetadataWithNotify(x, y, z, 0, 2);
+		}
+		
+		if (rotation == 1) {
+			world.setBlockMetadataWithNotify(x, y, z, 1, 2);
+		}
+		
+		if (rotation == 2) {
+			world.setBlockMetadataWithNotify(x, y, z, 2, 2);
+		}
+		
+		if (rotation == 3) {
+			world.setBlockMetadataWithNotify(x, y, z, 3, 2);
+		}
 	}
 	
 	@Override
@@ -65,5 +93,72 @@ public class MiningRig extends BlockContainer {
 		}
 		
 		super.breakBlock(world, x, y, z, block, meta);
+	}
+	
+	private static IIcon iconTopBack;
+	private static IIcon iconBottom;
+	private static IIcon iconFront;
+	private static IIcon iconSideL;
+	private static IIcon iconSideR;
+	
+	@Override
+	public IIcon getIcon(int side, int meta) {
+		if (side == 0) {
+			return iconBottom;
+		}
+		else if (side == 1) {
+			return iconTopBack;
+		}
+		
+		int[] sideOrder;
+		
+		switch (meta) {
+		case 0:
+			sideOrder = new int[] { 2, 5, 3, 4 };
+			break;
+		case 1:
+			sideOrder = new int[] { 5, 2, 4, 3 };
+			break;
+		case 2:
+			sideOrder = new int[] { 3, 5, 2, 4 };
+			break;
+		case 3:
+			sideOrder = new int[] { 4, 2, 5, 3 };
+			break;
+		default:
+			sideOrder = new int[] { 2, 5, 3, 4 };
+			break;
+		}
+		
+		int front = sideOrder[0];
+		int right = sideOrder[1];
+		int back = sideOrder[2];
+		int left = sideOrder[3];
+
+		if (side == back) {
+			return iconTopBack;
+		}
+		else if (side == front) {
+			return iconFront;
+		}
+		else if (side == left) {
+			return iconSideL;
+		}
+		else if (side == right) {
+			return iconSideR;
+		}
+		else {
+			return null;
+		}
+	}
+	
+	@Override
+	public void registerBlockIcons(IIconRegister icon) {
+		String name = DogeInfo.NAME.toLowerCase();
+		iconTopBack = icon.registerIcon(name + ":miningRigTopBack");
+		iconBottom = icon.registerIcon(name + ":miningRigBottom");
+		iconFront = icon.registerIcon(name + ":miningRigFront");
+		iconSideL = icon.registerIcon(name + ":miningRigSideL");
+		iconSideR = icon.registerIcon(name + ":miningRigSideR");
 	}
 }
